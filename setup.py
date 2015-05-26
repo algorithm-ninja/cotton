193c2f40d2c225dd5ab8bb2ff4b39390060426b9
William Di Luigi <williamdiluigi@gmail.com>
2015-05-27 00:40:32 +0200

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="cotton",
+    version="0.1",
+    author="algorithm-ninja",
+    author_email="algorithm@ninja",
+    url="https://github.com/algorithm-ninja/cotton"
+)
